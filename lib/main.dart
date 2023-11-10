c9868ad528d2c62c2fea41bf88083b9ae1170a39
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2023-11-10 11:40:13 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyFadeApp(),
+    );
+  }
+}
+
+class MyFadeApp extends StatefulWidget {
+  @override
+  _MyFadeAppState createState() => _MyFadeAppState();
+}
+
+class _MyFadeAppState extends State<MyFadeApp> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Create an animation controller
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2),
+    );
+
+    // Create a curved animation
+    _animation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    );
+
+    // Repeat the animation indefinitely
+    _controller.repeat(reverse: true);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('FadeTransition Example'),
+      ),
+      body: Center(
+        child: FadeTransition(
+          opacity: _animation,
+          child: Container(
+            width: 200,
+            height: 200,
+            color: Colors.blue,
+            child: Center(
+              child: Text(
+                'Fade In and Out',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
